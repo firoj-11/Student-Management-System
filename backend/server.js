2060b05add59c2d8fd0046cbfb1f6d6010007aac
firoj-11 <140113862+firoj-11@users.noreply.github.com>
2026-07-31 22:51:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const connectDB = require('./config/db');
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
