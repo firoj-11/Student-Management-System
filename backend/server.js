933bc28991d7b365c1b0dd85718572c141b6066f
firoj-11 <140113862+firoj-11@users.noreply.github.com>
2026-07-31 22:54:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+// ADD THESE TWO LINES TO FIX RENDER DNS ISSUES
 const dns = require('dns');
-const connectDB = require('./config/db');
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const connectDB = require('./config/db');
+
 const app = express();
 
 // Connect to MongoDB
